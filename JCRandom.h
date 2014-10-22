d30b3f8802f6574cb503dcf2fa402900727583d1
JCPedroza <nemesis4go10@hotmail.com>
2014-10-21 23:41:30 -0600

    implemented (double)rngZeroToOne and (double)rngZeroToOne(unsigned int)resolution

--- JCRandom.h
+++ JCRandom.h
@@ -7,17 +7,30 @@
 
 /**
  * Generates a random integer in the range [0, max].
- * @param max maximum value
- * @returns a random integer in the range [0, max]
+ * @param max Maximum value.
+ * @returns A random integer in the range [0, max].
  */
 - (int)rngInt:(int)max;
 
 /**
  * Generates a random integer in the range [min, max].
- * @param min minimum value
- * @param max maximum value
- * @returns a random integer in the range [min, max]
+ * @param min Minimum value.
+ * @param max Maximum value.
+ * @returns a random integer in the range [min, max].
  */
 - (int)rngIntFrom:(int)min to:(int)max;
+
+/**
+ * Generates a random double in the range [0, 1].
+ * @returns A random double in the range [0, 1].
+ */
+- (double)rngZeroToOne;
+
+/**
+ * Generates a random double in the range [0, 1], with a specific resolution.
+ * @param resolution Resolution of the range [0, 1].
+ * @returns A random doubel in the range [0, 1].
+ */
+- (double)rngZeroToOne:(unsigned int)resolution;
 
 @end
--- JCRandom.m
+++ JCRandom.m
@@ -1,6 +1,5 @@
 #import "JCRandom.h"
 
-
 @implementation JCRandom
 
 - (int)rngInt:(int)max {
@@ -9,6 +8,14 @@
 
 - (int)rngIntFrom:(int)min to:(int)max {
     return arc4random_uniform((max - min) + 1) + min;
+}
+
+- (double)rngZeroToOne {
+    return ((double)arc4random() / 0x100000000);
+}
+
+- (double)rngZeroToOne:(unsigned int)resolution {
+    return ((double)arc4random_uniform(resolution + 1) / resolution);
 }
 
 @end
